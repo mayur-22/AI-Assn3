948912cdf7dcf8dee7a52e52f2ecea464bec8a07
TANMAY912 <tanudim2012@gmail.com>
2019-10-07 19:01:55 +0530

    mapping from sat output

---
+++ out.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<fstream>
+#include <sstream>
+using namespace std;
+
+int main(){
+
+    ifstream file("test.temp");
+    int n,m;
+    file>>n>>m;
+    cout<<n<<" "<<m<<endl;
+    file.close();
+
+
+    ifstream rfile("test.satoutput");
+    string str;
+    getline(rfile,str);
+    cout<<str;
+
+
+    ofstream wfile("test.mapping");
+    if(str.compare("UNSAT")==0){
+        wfile<< "0";
+        wfile.close();
+        return 0;
+    }
+
+    getline(rfile,str);
+    // cout<<str<<endl;
+
+    stringstream ssin(str);
+    int i = 0;
+    string arr;
+    while (ssin.good() && i < n*m){
+      ssin >> arr;
+      i++;
+      // cout << arr << endl;
+      int x = stoi(arr);
+      if(x>0){
+        x--;
+        int a = x/m +1;
+        int b = x%m + 1;
+        wfile << to_string(a) << " " << to_string(b) << endl;
+
+      }
+    }
+
+    wfile.close();
+    rfile.close();
+
+
+}
