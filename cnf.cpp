7a9dabd8d6700f14f2cc25470f977a2c3efed727
Mayur Solanki <43881003+mayur-22@users.noreply.github.com>
2019-10-07 17:05:49 +0530

    Add files via upload

--- cnf.cpp
+++ cnf.cpp
@@ -29,7 +29,10 @@ int main()
 
 
 	}
-	// cout<<m<<n;
+
+	ofstream wfile("test.temp");
+	wfile<<to_string(n)<<" "<<to_string(m)<<"\n";
+	wfile.close();
 
 	int gEmail[n][n];
 	int gPhone[m][m];
@@ -98,11 +101,13 @@ int main()
 	int offset = n*m;
 	//trivial are true
 	for(int i=0;i<n;i++){
+		int k=1;
+		str = "";
 		for(int j=0;j<n;j++){
 			if(j!=i){
-				str = "";
-				int id = offset + i*n + j;
-				cout<<id<<" ";
+				int id = offset + i*(n-1) + k;
+				k++;
+				// cout<<id<<" "<<i<<" "<<j<<" \n";
 				if(gEmail[i][j]==0)
 					str += "-"+to_string(id)+" 0\n";
 				else
@@ -110,6 +115,81 @@ int main()
 			}
 		}
 		wrfile<<str;
+	}
+
+
+	offset += (n)*(n-1);
+	//trivial are true
+	for(int i=0;i<m;i++){
+		int k=1;
+		str = "";
+		for(int j=0;j<m;j++){
+			if(j!=i){
+				int id = offset + i*(m-1) + k;
+				k++;
+				// cout<<id<<" ";
+				if(gPhone[i][j]==0)
+					str += "-"+to_string(id)+" 0\n";
+				else
+					str += to_string(id)+" 0\n";
+			}
+		}
+		wrfile<<str;
+	}
+
+
+	//graphs constraints
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			//binded i and j
+			int id1 = i*m+1+j;
+			str = "";
+			for(int k=0;k<n;k++){
+				if(k!=i){
+					for(int l=0;l<m;l++){
+						if(l!=j){
+							cout<<i<<j<<k<<l<<endl;
+							//binded k and l
+							int id2 = k*m+1+l;
+							int temp = n*m + i*(n-1);
+							if(i>k)
+								temp += k+1;
+							else
+								temp += k;
+
+							int temp2 = n*m + n*(n-1) + j*(m-1);
+							if(j>l)
+								temp2 += l+1;
+							else
+								temp2 += l;
+							cout<<id1<<" "<<id2<<" "<<temp<<" "<<temp2<<"\n";
+
+							if(gEmail[i][k]==1)
+								str += "-"+to_string(id1) + "-"+to_string(id2) + " "+to_string(temp2) + " 0\n";
+							else
+								str += "-"+to_string(id1) + "-"+to_string(id2) + " -"+to_string(temp2) + " 0\n";
+
+
+						}
+					}
+
+
+				}
+			}
+			wrfile<<str;
+		}
+	}
+
+
+	//no more than one is true
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			str = "";
+			for(int k=j+1;k<n;k++){
+				str += "-"+to_string(j*m+i+1) + " -"+to_string(k*m+i+1)+" 0\n";
+			}
+			wrfile<<str;
+		}
 	}
 
 
